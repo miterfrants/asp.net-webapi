2cb0f11f365575bfd60e83166fe5332b904416c5
Peter Huang <miterfrants@gmail.com>
2019-10-02 02:55:11 +0800

    first commit

---
+++ api/migration/dbo.members.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[members] (
+    [id]         INT            IDENTITY (1, 1) NOT NULL,
+    [email]      NVARCHAR (512) NOT NULL,
+    [password]   NVARCHAR (512) NOT NULL,
+    [created_at] DATETIME       CONSTRAINT [DF_members_created_at] DEFAULT (getdate()) NOT NULL,
+    [updated_at] DATETIME       NULL,
+    [deleted_at] DATETIME       NULL,
+    [login_at]   DATETIME       NULL,
+    [salt] NVARCHAR(512) NOT NULL, 
+    CONSTRAINT [PK_members] PRIMARY KEY CLUSTERED ([id] ASC)
+);
+
---
+++ api/src/App_Code/Controllers/TestingController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+// PN is Presentation Network
+namespace PN.Controllers
+{
+    [RoutePrefix("api/v1")]
+    public class TestingController : ApiController
+    {
+        // GET: api/Testing
+        [Route("testing")]
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2", "value3" };
+        }
+
+        // GET: api/Testing/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Testing
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Testing/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Testing/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
